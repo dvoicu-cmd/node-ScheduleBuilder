f597f598b9cc6d0dd3266d7d60670ed66167eac8
dvoicu <dvoicu00@gmail.com>
2022-11-15 17:50:37 -0500

    implemented Schedule class with the method trim() working as intended

--- App/main.js
+++ App/main.js
@@ -6,5 +6,8 @@ function addStudent(){
 }
 
 function schedule(){
-    new Schedule();
+    const testSchedule = new Schedule();
+    console.log(testSchedule.selectedTime);
+    testSchedule.trim(9.5, 12.0);
+    console.log(testSchedule.selectedTime);
 }
--- App/schedule.js
+++ App/schedule.js
@@ -1,18 +1,53 @@
-class Schedule {
-    /**
-     * This is the default constructor for a Schedule object
-     * It assigns a time
-     */
 
+/**
+ * The schedule class is used in managing time frames of a day.
+ * Time is organized into 30 min chunks.
+ */
+class Schedule {
+    _selectedTime = [];
 
     constructor(){
-        let hourVal = 8.5
-        let temp = [hourVal];
-        for(let i = 0; i <= 24; i++){ //25 timeValues
-            temp.push(hourVal+0.5);
+        this._selectedTime = generateHours();
+    }
+
+    get selectedTime(){
+        return this._selectedTime;
+    }
+
+    /**
+     * Cuts a subset of 30 min chunks from the current list
+     * Precondition: 1) Numbers from 8.5, 9.0, 9.5, ..., 21.0. 2) when the method is repeatedly called, there is no overlap in past calls.
+     * @param {*A Number that selects the element to start at} from 
+     * @param {*A Number that selects the element to end at} till 
+     */
+    trim(from, till){
+        let startIndex = this._selectedTime.indexOf(from);
+        let endIndex = this._selectedTime.indexOf(till)-1;
+        if(startIndex < endIndex){
+            let prt1 = this._selectedTime.splice(0, startIndex);
+            let prt2 = this._selectedTime.splice(endIndex, this._selectedTime.length-1);
+            this._selectedTime = prt1.concat(prt2);
         }
-        this.Time = temp;
 
     }
 
+    reset(){
+        this._selectedTime = generateHours();
+    }
+
+    /**
+     * Generates and returns the array [8.5, 9.0, 9.5, ... , 21.0].
+     */
+}
+
+function generateHours(){
+    let i = 0;
+    let chunk = 8.5;
+    const output = [];
+    while(i<=25){
+        output.push(chunk);
+        chunk += 0.5;
+        i++;
+    }
+    return output;
 }
