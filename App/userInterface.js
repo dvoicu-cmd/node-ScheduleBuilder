548aa393a6e49b7dc2f8965d5e3e2cbc1f101ef7
Dan Voicu <user@users-MacBook-Pro.local>
2023-01-25 14:31:57 -0500

    added pop up menu/html file schedule.html

--- App/userInterface.js
+++ App/userInterface.js
@@ -24,6 +24,13 @@ function addStudent(){
 }
 
 /**
+ * Pop up window
+ */
+function viewSchedule(){
+    window.open('/schedule.html','popUpWindow','heigh=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+}
+
+/**
  * 
  */
 function addShift(){
